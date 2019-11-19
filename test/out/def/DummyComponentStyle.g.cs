97ff948397d1e646afda547ec897c8314396a9ad
Martin Welsch <martin.wel@hotmail.de>
2019-11-20 00:37:13 +0100

    fix namespace

---
+++ test/out/def/DummyComponentStyle.g.cs
@@ -0,0 +1,13 @@
+namespace test.Components {
+	public static class DummyComponentStyle {
+
+		public static string Icon => "DummyComponent-a";
+		public static string Parent => "DummyComponent-b";
+		public static string Nested => "DummyComponent-c";
+		public static string Disabled => "DummyComponent-d";
+		public static string ElClass => "DummyComponent-e";
+		public static string Miltiple => "DummyComponent-f";
+		public static string Classes => "DummyComponent-g";
+		public static string Block => "DummyComponent-h";
+	}
+}
