79bc1f933599363fa5a27ea8820f75cec9543de6
Martin Welsch <martin.wel@hotmail.de>
2019-11-22 14:08:21 +0100

    fix regex

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function loadModules() {
             this.modules.push(module);
 
             var cssString = file.contents.toString();
-            const cssClassRegex = /\.([_A-Za-z0-9\-]+)/g;
+            const cssClassRegex = /\.([a-zA-Z0-9-]+)(?:\s{|\,\s*|[ ]+)/g;
             var outCssStr = "";
 
             var pos = 0;
